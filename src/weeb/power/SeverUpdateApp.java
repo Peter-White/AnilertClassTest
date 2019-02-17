f4d3da6b83b55edf2aa9ea955028ee4b2c19ba9e
FourLeafCoder <pwhitedeveloper@gmail.com>
2019-02-17 18:48:48 -0500

    obsoleteShowtimeChecker now in thread

--- src/weeb/power/SeverUpdateApp.java
+++ src/weeb/power/SeverUpdateApp.java
@@ -17,51 +17,42 @@ public class SeverUpdateApp {
 	public static void main(String[] args) {
 
 //		while (true) {
-//			
-//			new Thread() {
-//
-//				@Override
-//				public void run() {
-					SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-					Date today = new Date();
+			
+			new Thread() {
+
+				@Override
+				public void run() {
+					
 					Set<Showtime> showtimes = ShowtimeQuery.queryAllShowtimes();
 					
 					for (Showtime showtime : showtimes) {
-						// Get current datetime for selected theater
-						Theater curentTheater = TheaterQuery.queryTheater(showtime.getTheaterID());
-						TimeZone timeZone = TimeZone.getTimeZone(curentTheater.getTimezone());
-						dtFormat.setTimeZone(timeZone);
-						String dateFormat = dtFormat.format(today);
-						today = ShowtimeQuery.datetimeConvert(dateFormat);
-						
-						Date showDate = ShowtimeQuery.datetimeConvert(showtime.getDateTime());
-						
-						if(today.after(showDate)) {
-							ShowtimeQuery.deleteShowtime(showtime);
-						}
+						obsoleteShowtimeChecker(showtime);
 					}
 					
-//				}
-//				
-//			}.start();
+				}
+				
+			}.start();
 			
 //		}
 		
-//		SimpleDateFormat timeToDie = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-//		TimeZone timeZone = TimeZone.getTimeZone(new TheaterJSONQuery().getTimeZone(42.3912716, -71.1384544));
-//		timeToDie.setTimeZone(timeZone);
-//		Date date = new Date();
-//		System.out.println(timeToDie.format(date));
-//		
-//		Showtime showtime = ShowtimeQuery.queryShowtime(124);
-//		ShowtimeQuery.deleteShowtime(showtime);
+	}
+	
+	public static void obsoleteShowtimeChecker(Showtime showtime) {
+		SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+		Date today = new Date();
 		
-//		Date oldDate = new Date(2018-1900, 3, 12, 13, 16);
-//		Date today = new Date();
-//		
-//		System.out.println(today);
-//		System.out.println(today.after(oldDate));
+		// Get current datetime for selected theater
+		Theater curentTheater = TheaterQuery.queryTheater(showtime.getTheaterID());
+		TimeZone timeZone = TimeZone.getTimeZone(curentTheater.getTimezone());
+		dtFormat.setTimeZone(timeZone);
+		String dateFormat = dtFormat.format(today);
+		today = ShowtimeQuery.datetimeConvert(dateFormat);
 		
+		Date showDate = ShowtimeQuery.datetimeConvert(showtime.getDateTime());
+		
+		if(today.after(showDate)) {
+			ShowtimeQuery.deleteShowtime(showtime);
+		}
 	}
 
 }
